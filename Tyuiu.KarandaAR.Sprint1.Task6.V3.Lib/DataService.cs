3441e10a9b85736429c1b963869878765fc07270
KarandaAR <karandaalexandr@gmail.com>
2024-10-03 23:33:49 +0500

    таск 6

---
+++ Tyuiu.KarandaAR.Sprint1.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KarandaAR.Sprint1.Task6.V3.Lib
+{
+    public class DataService : ISprint1Task6V3
+    {
+        public string LastLetterWord(string value)
+        {
+            string[] words = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            // Собираем последние буквы каждого слова
+            string lastLetters = "";
+            foreach (string word in words)
+            {
+                lastLetters += word[word.Length - 1];  // Берём последнюю букву каждого слова
+            }
+
+            return lastLetters;
+        }
+    }
+}
